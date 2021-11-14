d3bf1a1bfa1456b527abc533b7abc1466e1a6c95
rajsriselvan-ca <73290591+rajsriselvan-ca@users.noreply.github.com>
2021-11-14 19:39:54 +0530

    login user validation

--- my-app/src/Api/login.js
+++ my-app/src/Api/login.js
@@ -4,3 +4,6 @@ import configData from '../config.json';
 export const registerLogin = (payload) => {
    return Axios.post(`${configData.SERVER_URL}/${'register'}`, payload);
 }
+export const getUsers = () => {
+   return Axios.get(`${configData.SERVER_URL}/${'getUsers'}`);
+}
--- my-app/src/Components/login-form.js
+++ my-app/src/Components/login-form.js
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, notification, Card } from 'antd';
-import { MailOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Tabs, Card, Spin } from 'antd';
+import { MailOutlined, LockOutlined, RightCircleFilled, LeftCircleFilled } from '@ant-design/icons';
 import Helmet from 'react-helmet';
 import '../Styles/login.css';
-import { registerLogin } from '../Api/login';
+import { registerLogin, getUsers } from '../Api/login';
 import moment from "moment";
 import { notificationContent } from '../Shared Files/notification';
 
 
 function Login() {
-    const [registerUser, setRegisterUser ] = useState();
-    const [registerPassword, setRegisterPassword ] = useState();
+    const [registerUser, setRegisterUser] = useState();
+    const [registerPassword, setRegisterPassword] = useState();
+    const [formType, setFormType] = useState("User-Login");
 
     const onFinish = () => {
-        const currentDate = moment().format("DD-MM-YYYY hh:mm A")
-        console.log('Received values of form: ',currentDate );
-        const registerPayload = {
-            username: registerUser,
-            password: registerPassword,
-            submission_date: currentDate
-        };
-        registerLogin(registerPayload).then((response) => {
-            const status = response.data;
-            notificationContent(status, "Registration");
-        });
+        const formCode = formType;
+        if (formCode === "User-Login") {
+            getUsers().then(response => {
+                const status = response.data;
+                const checkTheUser = response.data.filter(e => e.user_name === registerUser && e.user_password === registerPassword);
+                if (checkTheUser.length === 0) {
+                    return notificationContent("error", "Login");
+                } else {
+                    return notificationContent("success", "Login");
+                }
+            });
+        }
+        else if (formCode === "User-Registration") {
+            const currentDate = moment().format("DD-MM-YYYY hh:mm A")
+            const registerPayload = {
+                username: registerUser,
+                password: registerPassword,
+                submission_date: currentDate
+            };
+            registerLogin(registerPayload).then((response) => {
+                const status = response.data;
+                if (status === "success") setFormType("User-Login");
+                notificationContent(status, "Registration");
+            });
+        }
     };
     return (
         <div className="login-outline">
@@ -34,13 +49,13 @@ function Login() {
                     <Form
                         name="normal_login"
                         className="login-form"
+                        onFinish={onFinish}
                         initialValues={{
                             remember: true,
                         }}
-                        onFinish={onFinish}
                     >
                         <div className="inner-box">
-                            <span className="login-header"><b>Sign Up</b></span>
+                            <span className="login-header">{formType !== "User-Login" ? <b>Registration</b> : <b>Login</b>}</span>
                             <div>
                                 <Form.Item
                                     name="username"
@@ -73,10 +88,17 @@ function Login() {
                                     />
                                 </Form.Item>
                                 <Form.Item>
-                                    <Button type="primary" htmlType="submit" className="login-form-button">
-                                        Register
+                                    <Button type="primary" htmlType="submit" className="login-form-button" >
+                                        {formType !== "User-Login" ? "Register" : "Login"}
                                     </Button>
                                 </Form.Item>
+                                <div>{formType === "User-Login" ? (<div>Are you a new user ? Please register here {<RightCircleFilled
+                                    onClick={(event) => {
+                                        setFormType("User-Registration");
+                                    }} style={{ color: "#26afeb", fontSize: "16px", cursor: "pointer" }} />}</div>) :
+                                    (<div className="register-back-button" onClick={event => setFormType("User-Login")}><LeftCircleFilled style={{ color: "#26afeb", fontSize: "20px", marginTop: "20px" }} />
+                                        <span style={{ color: "#636f80", fontSize: "18px" }}><b> Go Back</b></span></div>)}
+                                </div>
                             </div>
                         </div>
                     </Form>
