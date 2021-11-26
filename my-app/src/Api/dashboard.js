9e5772c9624caa2c6f66165771500082e71327ec
rajsriselvan-ca <73290591+rajsriselvan-ca@users.noreply.github.com>
2021-11-27 03:37:17 +0530

    dropdown data

--- my-app/src/Api/dashboard.js
+++ my-app/src/Api/dashboard.js
@@ -4,3 +4,6 @@ import configData from '../config.json';
 export const getNotesType = () => {
     return Axios.get(`${configData.SERVER_URL}/${'dashboard/notes-type'}`);
  }
+export const getProgramLanguage = () => {
+    return Axios.get(`${configData.SERVER_URL}/${'dashboard/get-program-language'}`);
+ }
--- my-app/src/Components/modal.js
+++ my-app/src/Components/modal.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Form, Input, Button, Card, Modal, Select, Row, Col } from 'antd';
 import "../Styles/modal.css"
+import { getNotesType, getProgramLanguage } from "../Api/dashboard";
 
 const { Option } = Select;
 const { TextArea } = Input;
@@ -8,6 +9,34 @@ const { TextArea } = Input;
 
 function FormDetails(props) {
     const [form] = Form.useForm();
+    const [getCategory, setCategory] = useState([]);
+    const [getProgramType, setProgramType] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const incomingCategory = await getNotesType();
+            const incomingLanguage = await getProgramLanguage();
+            setCategory(incomingCategory.data);
+            setProgramType(incomingLanguage.data);
+        }
+        fetchData();
+    }, []);
+
+    function onChange(value) {
+        return value
+    }
+
+    function onBlur() {
+        return
+    }
+
+    function onFocus() {
+        return
+    }
+
+    function onSearch(val) {
+        return val;
+    }
 
     const handleCancel = () => {
         props.cancel(false);
@@ -15,7 +44,6 @@ function FormDetails(props) {
 
     return (
         <>
-            {console.log("comeing--->>", props.data)}
             <Modal title="Create Notes" className="popup-frame" width="60%" visible={props.data} onCancel={handleCancel} >
                 <Form
                     className="notes-form"
@@ -40,7 +68,7 @@ function FormDetails(props) {
                                     <Input />
                                 </Form.Item>
                             </Col>
-                            <Col className="gutter-row" span={10} offset={3}> 
+                            <Col className="gutter-row" span={10} offset={3}>
                                 <Form.Item
                                     name="gender"
                                     label="Category"
@@ -51,16 +79,53 @@ function FormDetails(props) {
                                     ]}
                                 >
                                     <Select
-                                        placeholder="Select"
-                                        // onChange={onGenderChange}
-                                        allowClear
+                                        showSearch
+                                        style={{ width: 200 }}
+                                        placeholder="Select Notes Type"
+                                        optionFilterProp="children"
+                                        onChange={onChange}
+                                        onFocus={onFocus}
+                                        onBlur={onBlur}
+                                        onSearch={onSearch}
+                                        filterOption={(input, option) =>
+                                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                        }
                                     >
-                                        <Option value="male">male</Option>
-                                        <Option value="female">female</Option>
-                                        <Option value="other">other</Option>
+                                        {getCategory.map(record => (
+                                            <Option value={record.notes_type_id}>{record.notes_type}</Option>
+                                        ))}
                                     </Select>
                                 </Form.Item>
                             </Col>
+                        </Row>
+                        <Row>
+                        <Form.Item 
+                                    name="Type"
+                                    label="Language"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                            <Select
+                                showSearch
+                                style={{ width: 200 }}
+                                placeholder="Select language"
+                                optionFilterProp="children"
+                                onChange={onChange}
+                                onFocus={onFocus}
+                                onBlur={onBlur}
+                                onSearch={onSearch}
+                                filterOption={(input, option) =>
+                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                            >
+                                {getProgramType.map(record => (
+                                    <Option value={record.language_id}>{record.language_name}</Option>
+                                ))}
+                            </Select>
+                            </Form.Item>
                         </Row>
                         <Form.Item
                             name="text-area"
@@ -72,7 +137,7 @@ function FormDetails(props) {
                             ]}
 
                         >
-                            <TextArea rows={8} style={{backgroundColor:"#222211", color:"#cccccc"}} />
+                            <TextArea rows={6} style={{ backgroundColor: "#222211", color: "#cccccc" }} />
                         </Form.Item>
                     </div>
                 </Form>
